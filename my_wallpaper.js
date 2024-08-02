ab93b2404cb4716f4430ffe0c4c154d200350120
Hermione <Hermionej1005@gmail.com>
2024-08-02 15:09:40 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -6,10 +6,17 @@
 
 //your parameter variables go here!
 
-let cat_fur_color = "#fffff";
-// if statwmwnr for turning on aor off whiskers
+let cat_fur_color = "#fffff";  // Change the color of the cat's face
 
+let cat_whiskers = true  // if statement for turning on or off whiskers
 
+let cat_blush = true // if statement for turning on or off blush
+
+let cat_eye_colour = "#5ebad1"  //Change the color of the cat's eye
+
+let fish = true  // if statement for turning on or off fish
+
+let rhomboid = false // if statement for turning on or off rhomboid
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -23,7 +30,7 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(220, 255, 200); //light honeydew green colour
+  background(244, 245, 220); //light honeydew green colour
    // Three-digit hex RGB notation.
    
    fill('#fae');
@@ -33,65 +40,92 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  // 设置猫脸中心位置
-  centerX = 100; // 固定中心X坐标
-  centerY = 100; // 固定中心Y坐标
-
-  // 画耳朵
-  fill(80); // 浅橙色
-  triangle(71, 110, 85, 53, 109, 75);
-  triangle(169, 110, 135, 73, 160, 55);
   
-  // face
-  fill(cat_fur_color); // 浅橙色
-  ellipse(120, 120, 100, 100); // 猫脸
 
-  // eye
-  fill(300);
-  ellipse(100, 120, 20, 30); // L
-  ellipse(140, 120, 20, 30); // R
-  //pupil
-  fill(50);
-  ellipse(98, 118, 15, 19); // L
-  ellipse(138, 118, 15, 19); // R
-  //Highlights
-  fill(300)
-  ellipse(95, 119, 3, 6); //L
-  ellipse(135, 119, 3, 6);//R
-  
-  // 画鼻子
-  fill('#fae');
-  triangle(113, 130, 120, 140, 128, 130);// 鼻子
-
-  // 画嘴巴
-  //fill(50);
-  line(120, 140, 115, 150); // 左半边嘴
-  line(120, 140, 125, 150); // 右半边嘴
- 
+  if (cat_whiskers){
   //Left whiskers
-  fill(50);
-  line(55,110,80,120);
-  line(55,130,80,130);
-  line(57,150,80,140);
+  fill(20);
+  line(55,110,80,120);//top
+  line(55,130,80,130);//middle
+  line(57,150,80,140);//nether
 
   //Right whiskers
-  fill(50);
-  line(160,120,185,110);
-  line(160,130,185,130);
-  line(160,140,185,150);
+  fill(20);
+  line(160,120,185,110);//top
+  line(160,130,185,130);//middle
+  line(160,140,185,150);//nether
+  }
+
   
+  // Ear
+  noStroke();
+  fill(80); //gray
+  triangle(71, 110, 85, 53, 109, 75);//Left
+  triangle(169, 110, 135, 73, 160, 55);//Right
+  
+  // Face
+  fill(cat_fur_color); //colour
+  ellipse(120, 120, 100, 100); 
+
+  // Mouth
+  fill(50);//black
+  line(120, 140, 115, 150); // Left
+  line(120, 140, 125, 150); // Righ
+
+  // Eye
+  fill(cat_eye_colour);
+  ellipse(100, 120, 20, 30); // Left
+  ellipse(140, 120, 20, 30); // Right
+  //Pupil
+  fill(50);
+  ellipse(98, 118, 15, 19); // Left
+  ellipse(138, 118, 15, 19); // Right
+  //Highlights
+  fill(300)
+  ellipse(95, 119, 3, 6); //Left
+  ellipse(135, 119, 3, 6);//Right
+  
+  // Nose
+  fill(255, 182, 193);//Light pink
+  triangle(113, 130, 120, 140, 128, 130);
+
+  // Mouth
+  fill(50);//black
+  line(120, 140, 115, 150); // Left
+  line(120, 140, 125, 150); // Right
+ 
+  if (fish){
+  //Fish
+  noStroke();
+  fill(300);
+  ellipse(60, 25, 70, 30);
+  triangle(10, 10, 35, 25, 10, 40);
+  fill(200);
+  ellipse(60, 22.2, 66, 24);
+  fill(20);
+  ellipse(83, 25, 5);
+  }
+  
+
+  if (rhomboid){
   fill(255, 204, 100);
   quad(50, 80, 70, 50, 50, 20, 30, 50);
-  //fillPolygon(xPointsLeftEar, yPointsLeftEar, 3); // 左耳朵
+  }
 
-  //ellipse( 250, 230, 240 );
-  //ellipse( 150, 150, 120 );
-  
-  // 画脸颊腮红
- // fill(255, 182, 193); // 浅粉色
- // ellipse(155, 200, 20, 10); // 左脸颊
- // ellipse(225, 200, 20, 10); // 右脸颊
+
+  if (cat_blush){
+  // Cheek blush
+  fill(255, 182, 193); // Light pink
+  ellipse(95, 140, 20, 10); // Left
+  ellipse(145, 140, 20, 10); // Right
+  }
+
+
+
+
+
 }
+
 
  
   
